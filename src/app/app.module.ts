c7aa75ee576ac4942c67c89fb940667abedf9393
Supansa <s6201012620236@email.kmutnb.ac.th>
2021-10-13 14:25:16 +0700

    install font awesome and bootstrap icons

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
 import { LayoutComponent } from './components/layout/layout.component';
+import { NavigationBarComponent } from './components/layout/navigation-bar/navigation-bar.component';
+import { BreadcrumbComponent } from './components/layout/breadcrumb/breadcrumb.component';
+import { NavbarComponent } from './components/layout/navbar/navbar.component';
 import { AssignShiftComponent } from './components/assign-shift/assign-shift.component';
 import { AssignOTComponent } from './components/assign-ot/assign-ot.component';
 import { HttpClientModule } from '@angular/common/http';
@@ -12,6 +16,9 @@ import { HttpClientModule } from '@angular/common/http';
   declarations: [
     AppComponent,
     LayoutComponent,
+    NavigationBarComponent,
+    NavbarComponent,
+    BreadcrumbComponent,
     AssignShiftComponent,
     AssignOTComponent
   ],
