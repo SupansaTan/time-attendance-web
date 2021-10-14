fe2f95feac106f305690f9a5d2f2424b1aa71a4c
Supansa <s6201012620236@email.kmutnb.ac.th>
2021-10-15 02:18:27 +0700

    add navigation bar

---
+++ src/app/components/layout/menu-items.ts
@@ -0,0 +1,31 @@
+export default {
+  items: [
+    {
+      id: 'dashboard',
+      title: 'Dashboard',
+      type: 'item',
+      url: '/dashboard',
+      icon: 'bi bi-display'
+    },
+    {
+      id: 'assign-plan',
+      title: 'Assign Plan',
+      type: 'collapse',
+      icon: 'bi bi-pen',
+      children: [
+        {
+          id: 'assgin-shift',
+          title: 'Assign Shift',
+          type: 'item',
+          url: '#'
+        },
+        {
+          id: 'assgin-ot',
+          title: 'Assign OT',
+          type: 'item',
+          url: '#'
+        }
+      ]
+    }
+  ]
+}
--- src/app/components/layout/navigation-bar/navigation-bar.component.ts
+++ src/app/components/layout/navigation-bar/navigation-bar.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
+import { MenuItems } from 'src/app/model/menu-items.model';
+import navigation from '../menu-items';
 
 @Component({
   selector: 'app-navigation-bar',
@@ -6,10 +8,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navigation-bar.component.scss']
 })
 export class NavigationBarComponent implements OnInit {
+  menus: Array<MenuItems>
+  navCollapsed: boolean
+  menuCollapsed: boolean
+  public screenWidth: any;
 
   constructor() { }
 
   ngOnInit(): void {
+    this.menus = navigation.items
+    this.screenWidth = window.innerWidth;
+    this.menuCollapsed = false;
   }
 
+  @HostListener('window:resize', ['$event'])
+  onResize() {
+    this.screenWidth = window.innerWidth;
+  }
 }
