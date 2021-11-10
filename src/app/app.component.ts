75473106dfc25cac410997b6a73c889700dda2fa
Supansa <s6201012620236@email.kmutnb.ac.th>
2021-11-11 00:39:10 +0700

    config route for authentication

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'time-attendance';
+  path: string
+
+  constructor() {
+    this.path = location.pathname.split("/")[1]
+  }
 }
--- src/app/components/authentication/authentication-routing.module.ts
+++ src/app/components/authentication/authentication-routing.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ConfirmRegisterComponent } from './confirm-register/confirm-register.component';
 import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
 
 const routes: Routes = [
@@ -8,8 +10,14 @@ const routes: Routes = [
     path: 'login',
     component: LoginComponent
   },
-
-
+  {
+    path: 'register',
+    component: RegisterComponent
+  },
+  {
+    path: 'register/success',
+    component: ConfirmRegisterComponent
+  },
 ];
 @NgModule({
   imports: [RouterModule.forChild(routes)],
