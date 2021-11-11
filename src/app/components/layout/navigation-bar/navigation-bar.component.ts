df69ac92c2caf706b2d9efd77808765f5408cc3d
Supansa <s6201012620236@email.kmutnb.ac.th>
2021-11-12 01:14:16 +0700

    get user role from auth service

--- src/app/components/layout/navigation-bar/navigation-bar.component.ts
+++ src/app/components/layout/navigation-bar/navigation-bar.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, HostListener } from '@angular/core';
 import { MenuItems } from 'src/app/model/menu-items.model';
+import { AuthenticationService } from '../../authentication/authentication.service';
 import navigation from '../menu-items';
 
 @Component({
@@ -11,14 +12,17 @@ export class NavigationBarComponent implements OnInit {
   menus: Array<MenuItems>
   navCollapsed: boolean
   menuCollapsed: boolean
+  role: string
   public screenWidth: any;
 
-  constructor() { }
+  constructor(private authService: AuthenticationService) { }
 
   ngOnInit(): void {
     this.menus = navigation.items
     this.screenWidth = window.innerWidth;
     this.menuCollapsed = false;
+    this.role = this.authService.getUserRole()
+    this.role == 'employee'? this.menus.pop() : false  // not show assign plan when user is employee
   }
 
   @HostListener('window:resize', ['$event'])
