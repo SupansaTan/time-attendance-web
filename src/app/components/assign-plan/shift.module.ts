aab7ed68dfefd873ec6bf37b704220d328d4f54d
Supansa <s6201012620236@email.kmutnb.ac.th>
2021-11-15 18:04:52 +0700

    install angular material

--- src/app/components/assign-plan/shift.module.ts
+++ src/app/components/assign-plan/shift.module.ts
@@ -2,6 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { NgSelectModule } from '@ng-select/ng-select';
+import { ReactiveFormsModule } from '@angular/forms';
+
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatNativeDateModule } from '@angular/material/core';
+import { NgxMaterialTimepickerModule } from 'ngx-material-timepicker';
 
 import { AssignPlanComponent } from './assign-plan.component';
 import { PlanDetailComponent } from './plan-detail/plan-detail.component';
@@ -9,7 +17,7 @@ import { ShiftRoutingModule } from './shift-routing.module';
 
 
 @NgModule({
-  declarations: [  
+  declarations: [
     AssignPlanComponent,
     PlanDetailComponent
   ],
@@ -18,6 +26,13 @@ import { ShiftRoutingModule } from './shift-routing.module';
     HttpClientModule,
     FormsModule,
     ShiftRoutingModule,
+    NgbModule,
+    NgSelectModule,
+    ReactiveFormsModule,
+    MatDatepickerModule,
+    MatFormFieldModule,
+    MatNativeDateModule,
+    NgxMaterialTimepickerModule.setLocale('th-TH')
   ]
 })
 export class ShiftModule { }
