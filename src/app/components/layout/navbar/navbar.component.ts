bf9314cf801d17700c0007064cf9a20f8f625a0d
natthamon55 <natthamon2012@hotmail.com>
2021-11-17 01:21:35 +0700

    add alert when logout

--- src/app/components/layout/navbar/navbar.component.ts
+++ src/app/components/layout/navbar/navbar.component.ts
@@ -29,7 +29,7 @@ export class NavbarComponent implements OnInit {
   }
 
    logout(): void {
-    console.log("Exit")
+    alert("Logout Time-attendance")
     this.router.navigate(['/auth/login']);
   }
 }
